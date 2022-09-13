9d69764e2ef33bcd0276d61ee2f8e8739d08ec43
Jan Nowak <nowak.jan@yahoo.com>
2022-09-13 14:19:44 +0200

    add login and logout routes

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -1,8 +1,22 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/user.models.js');
 
+exports.loginUser = async (req, res) => {
+    res.send(req.user);
+}
+
+exports.logoutUser = async (req, res) => {
+    console.log(req.user)
+    req.logout((err) => {
+        if(err)
+            throw(err);
+        });
+        res.send("Successfully logged out");
+}
+
 exports.getUsers = async (req, res) => {
     try{
+        console.log(req.user);
         const users = await User.find();
         res.status(200).json({ success: true, msg: users});
     }
@@ -14,16 +28,17 @@ exports.getUsers = async (req, res) => {
 exports.createUser = async (req, res) => {
     const {username, password, email, profileImage, role} = req.body;
     const user = new User({ username, password, email, profileImage, role});
-    bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(user.password, salt, (err, hash) => {
-            if(err)
-                throw err;
-            user.password = hash;
-        });
-    });
     try{
-        await user.save();
-        res.status(201).json({ success: true, msg: user});
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, async (err, hash) => {
+                if(err){
+                    throw err;
+                }
+                user.password = hash;
+                await user.save();
+                res.status(201).json({ success: true, msg: user});
+            })
+        });
     }
     catch(err){
         res.status(409).json({ success: false, msg: err.message || "Unknown error"});
@@ -47,7 +62,3 @@ exports.updateUser = (req, res) => {
 exports.deleteUser = (req, res) => {
     res.send('SUCCESS');
 };
-
-exports.loginUser = async (req, res) => {
-    res.send(req.user);
-}
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const passport = require('passport');
-const { getUsers, createUser, getOneUser, updateUser, deleteUser, loginUser } = require('../controllers/user.controllers.js');
+const { getUsers, createUser, getOneUser, updateUser, deleteUser, loginUser, logoutUser } = require('../controllers/user.controllers.js');
 
 const router = express.Router();
 
 router.route('/')
     .get(getUsers)
 
+router.route('/login')
+    .post(passport.authenticate('local', {failureRedirect: '/users/login'}),  loginUser)
+
+router.route('/logout')
+    .get(logoutUser);
+
+router.route('/register')
+    .post(createUser);
+
 router.route('/:id')
     .get(getOneUser)
     .patch(updateUser)
     .delete(deleteUser)
-
-router.route('/login')
-    .post(passport.authenticate('local', {failureRedirect: '/login'}),  loginUser)
-
-router.route('/register')
-    .post(createUser)
 
 module.exports = router;
